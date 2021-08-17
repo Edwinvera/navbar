619b87301a58a5a6a39816b0e76199b905517cdb
edwinvera <edwinverafitness@gmail.com>
2021-08-17 11:28:00 -0400

    Add toggle

--- app.js
+++ app.js
@@ -3,3 +3,19 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const button = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
+
+button.addEventListener('click', function () {
+  // console.log(links.classList)
+  // console.log(links.classList.contains('random'))
+  // console.log(links.classList.contains('links'))
+  // if (links.classList.contains("show-links")) {
+  //   links.classList.remove("show-links")
+  // } else {
+  //   links.classList.add("show-links")
+  // }
+  //One line below!
+  links.classList.toggle("show-links")
+})
